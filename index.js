d0325a52622c547951be8d845f4443c51ae082cc
Jackson Lawrence Crasta <jlcrasta@protonmail.com>
2022-04-24 21:11:03 +0530

    changed env

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express')
-require('dotenv').config()
+//require('dotenv').config()
 const app = express()
 const mongoose = require('mongoose')
 const createUser = require('./Routes/createUser')
